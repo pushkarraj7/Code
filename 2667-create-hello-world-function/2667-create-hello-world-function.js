488eea1b550ea26a7ab31c9e715acd9c3c461482
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-05-07 08:55:38 +0530

    Time: 63 ms (18.66%), Space: 41.6 MB (63.28%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,13 @@
+/**
+ * @return {Function}
+ */
+function createHelloWorld() {
+  return function() {
+    return "Hello World";
+  }
+}
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
