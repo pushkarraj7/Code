79084c5959d88dd149cc14b14f6641599fb4532b
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-05-10 13:21:54 +0530

    Time: 9 ms (9.11%), Space: 42.6 MB (62.34%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minDistance(String str1, String str2) {
+        int n=str1.length();
+        int m=str2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0)
+                    dp[i][j]=j;
+                if(j==0)
+                    dp[i][j]=i;
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    int add=dp[i][j-1]+1;
+                    int del=dp[i-1][j]+1;
+                    int rep=dp[i-1][j-1]+1;
+                    dp[i][j]=Math.min(add, Math.min(del,rep));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
