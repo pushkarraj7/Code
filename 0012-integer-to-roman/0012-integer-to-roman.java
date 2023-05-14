bc5f7fbeab733d1f2ac6470b8872864cb536fbc1
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-05-14 12:38:13 +0530

    Time: 4 ms (100.00%), Space: 42.6 MB (58.73%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (num > 0) {
+            if (num - values[i] >= 0) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            } else {
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+}
