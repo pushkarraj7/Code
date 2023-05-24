203c51d30ab2dfc4a2aec7c4694e75e05dc3c464
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-05-24 18:04:30 +0530

    Time: 6 ms (57.84%), Space: 42.4 MB (82.04%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxLen = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }        
+        return maxLen;
+    }
+}
