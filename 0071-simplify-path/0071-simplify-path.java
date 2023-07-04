615ae31bc717652683aa0fc2fe68e581c79da252
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-07-04 15:19:43 +0530

    Time: 4 ms (92.87%), Space: 42.3 MB (94.53%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,26 +1,81 @@
+// chatgpt
+// class Solution {
+//     public String simplifyPath(String path) {
+//         Deque<String> stack = new LinkedList<>();
+//         String[] components = path.split("/");
+        
+//         for (String component : components) {
+//             if (component.equals("..")) {
+//                 if (!stack.isEmpty()) {
+//                     stack.pop();
+//                 }
+//             } else if (!component.isEmpty() && !component.equals(".")) {
+//                 stack.push(component);
+//             }
+//         }
+//         StringBuilder simplifiedPath = new StringBuilder();
+//         while (!stack.isEmpty()) {
+//             simplifiedPath.append("/").append(stack.pollLast());
+//         }
+        
+//         if (simplifiedPath.length() == 0) {
+//             simplifiedPath.append("/");
+//         }
+        
+//         return simplifiedPath.toString();
+//     }
+// }
+
+
+//https://www.youtube.com/watch?v=8fM6D3sMIDs
+
+//Stack
+//SC -> O(n)
+//TC -> O(n)
+// class Solution {
+//     public String simplifyPath(String path){
+//         Stack<String> s = new Stack<>();
+//         StringBuilder res = new StringBuilder();
+//         String[] p = path.split("/");
+        
+//         for(int i =0 ; i < p.length; i++){
+//             if(!s.isEmpty() && p[i].equals(".."))
+//                 s.pop();
+//             else if (!p[i].equals("") && !p[i].equals(".") && !p[i].equals(".."))
+//                 s.push(p[i]);
+//         }
+        
+//         if(s.isEmpty()) return "/";
+//         while(!s.isEmpty()){
+//             res.insert(0, s.pop()).insert(0, "/");
+//         }
+        
+//         return res.toString();
+//     }
+// }
+
+
+//Deque
+//SC -> O(n)
+//TC -> O(n)
 class Solution {
-    public String simplifyPath(String path) {
-        Deque<String> stack = new LinkedList<>();
-        String[] components = path.split("/");
+    public String simplifyPath(String path){
+        Deque<String> s = new LinkedList<>();
+        StringBuilder res = new StringBuilder();
+        String[] p = path.split("/");
         
-        for (String component : components) {
-            if (component.equals("..")) {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
-            } else if (!component.isEmpty() && !component.equals(".")) {
-                stack.push(component);
-            }
-        }
-        StringBuilder simplifiedPath = new StringBuilder();
-        while (!stack.isEmpty()) {
-            simplifiedPath.append("/").append(stack.pollLast());
+        for(int i =0 ; i < p.length; i++){
+            if(!s.isEmpty() && p[i].equals(".."))
+                s.poll();
+            else if (!p[i].equals("") && !p[i].equals(".") && !p[i].equals(".."))
+                s.push(p[i]);
         }
         
-        if (simplifiedPath.length() == 0) {
-            simplifiedPath.append("/");
+        if(s.isEmpty()) return "/";
+        while(!s.isEmpty()){
+            res.append("/").append(s.pollLast());
         }
         
-        return simplifiedPath.toString();
+        return res.toString();
     }
 }
