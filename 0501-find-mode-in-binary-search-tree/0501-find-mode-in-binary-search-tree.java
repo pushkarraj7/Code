7228422949cdbfd4f69cd3404417fc1ece4be71b
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-11-01 13:27:56 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (70.94%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int currentVal;
+    private int currentCount;
+    private int maxCount;
+    private List<Integer> modes;
+
+    public int[] findMode(TreeNode root) {
+        currentVal = 0;
+        currentCount = 0;
+        maxCount = 0;
+        modes = new ArrayList<>();
+
+        inOrderTraversal(root);
+
+        int[] result = new int[modes.size()];
+        for (int i = 0; i < modes.size(); i++) {
+            result[i] = modes.get(i);
+        }
+
+        return result;
+    }
+
+    private void inOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrderTraversal(node.left);
+
+        if (node.val == currentVal) {
+            currentCount++;
+        } else {
+            currentVal = node.val;
+            currentCount = 1;
+        }
+
+        if (currentCount > maxCount) {
+            maxCount = currentCount;
+            modes.clear();
+            modes.add(currentVal);
+        } else if (currentCount == maxCount) {
+            modes.add(currentVal);
+        }
+
+        inOrderTraversal(node.right);
+    }
+}
