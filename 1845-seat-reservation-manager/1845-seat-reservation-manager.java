2c967d7578b9b69adbc31e7aa616076afa806f57
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-11-06 23:22:55 +0530

    Time: 79 ms (52.87%), Space: 86.3 MB (52.87%) - LeetHub

---
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.java
@@ -0,0 +1,29 @@
+class SeatManager {
+    private PriorityQueue<Integer> availableSeats;
+
+    public SeatManager(int n) {
+        availableSeats = new PriorityQueue<>();
+        for (int i = 1; i <= n; i++) {
+            availableSeats.offer(i);
+        }
+    }
+
+    public int reserve() {
+        if (!availableSeats.isEmpty()) {
+            return availableSeats.poll();
+        } else {
+            return -1; // No available seats.
+        }
+    }
+
+    public void unreserve(int seatNumber) {
+        availableSeats.offer(seatNumber);
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */
