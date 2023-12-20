c16419a7ab2c0e5b0dd69a3a3cf82dbb890d0671
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-12-20 11:13:33 +0530

    Time: 3592 ms (5.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select distinct customer_id
+    from (
+        select customer_id, count(distinct product_key) as product_count
+            from Customer
+            group by customer_id
+    ) as customer_product_counts
+    join (
+        select count(distinct product_key) as product_count from Product
+    ) as product_counts
+    on customer_product_counts.product_count = product_counts.product_count;
