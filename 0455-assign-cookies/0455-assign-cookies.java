bdc483486088ac2317028ce306381131bf71857a
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-12-22 15:45:02 +0530

    Time: 8 ms (98.72%), Space: 45.3 MB (6.20%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int contentChildren = 0;
+        int i = 0; // Index for children
+        int j = 0; // Index for cookies
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                contentChildren++;
+                i++;
+            }
+            j++;
+        }
+
+        return contentChildren;
+    }
+}
