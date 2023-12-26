87af15f9f6f8a2f8ba3bd9f3b28dd98a2674300e
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-12-26 12:08:26 +0530

    Time: 23 ms (26.51%), Space: 41.6 MB (29.72%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    private static final int MOD = 1000000007;
+
+    public int numRollsToTarget(int n, int k, int target) {
+        // dp[i][j] represents the number of ways to get a sum of j using i dice
+        int[][] dp = new int[n + 1][target + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                for (int face = 1; face <= k; face++) {
+                    if (j >= face) {
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - face]) % MOD;
+                    }
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+}
