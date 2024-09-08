6fd713c621510247f280dfa7c729ad609338aa6e
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2024-09-08 20:30:06 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (52.73%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -18,28 +18,23 @@ class Solution {
             length++;
             current = current.next;
         }
-
         // Calculate the size of each part and the number of remaining elements
         int partSize = length / k;
         int remaining = length % k;
 
         current = head;
         ListNode[] result = new ListNode[k];
-
         // Traverse the linked list and split it into parts
         for (int i = 0; i < k && current != null; i++) {
             result[i] = current;
-
             int partLength = partSize + (i < remaining ? 1 : 0); // Adjust part size for remaining elements
             for (int j = 0; j < partLength - 1; j++) {
                 current = current.next;
             }
-
             ListNode next = current.next;
             current.next = null;
             current = next;
-        }
-
+        }         
         return result;
     }
 }
