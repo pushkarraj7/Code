059993b6f14f6657f786a293d322aefd88af1ebc
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2024-10-23 14:47:58 +0530

    Time: 45 ms (33.47%), Space: 90.1 MB (21.19%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public TreeNode replaceValueInTree(TreeNode root) {
+    List<Integer> levelSums = new ArrayList<>();
+    dfs(root, 0, levelSums);
+    return replace(root, 0, new TreeNode(0), levelSums);
+  }
+
+  private void dfs(TreeNode root, int level, List<Integer> levelSums) {
+    if (root == null)
+      return;
+    if (levelSums.size() == level)
+      levelSums.add(0);
+    levelSums.set(level, levelSums.get(level) + root.val);
+    dfs(root.left, level + 1, levelSums);
+    dfs(root.right, level + 1, levelSums);
+  }
+
+  private TreeNode replace(TreeNode root, int level, TreeNode curr, List<Integer> levelSums) {
+    final int nextLevel = level + 1;
+    final int nextLevelCousinsSum = nextLevel >= levelSums.size()
+                                        ? 0
+                                        : levelSums.get(nextLevel) -
+                                              (root.left == null ? 0 : root.left.val) -
+                                              (root.right == null ? 0 : root.right.val);
+    if (root.left != null) {
+      curr.left = new TreeNode(nextLevelCousinsSum);
+      replace(root.left, level + 1, curr.left, levelSums);
+    }
+    if (root.right != null) {
+      curr.right = new TreeNode(nextLevelCousinsSum);
+      replace(root.right, level + 1, curr.right, levelSums);
+    }
+    return curr;
+  }
+}
