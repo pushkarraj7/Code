70b1e8ad24de14c2eb5590bb4cd592d93103b9a9
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2024-10-28 10:22:19 +0530

    Time: 31 ms (90.63%), Space: 62.5 MB (16.67%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
@@ -1,22 +1,30 @@
 class Solution {
     public int longestSquareStreak(int[] nums) {
         Set<Integer> uniques = new HashSet<>();
-        for (int num: nums) {
+        for (int num : nums) {
             uniques.add(num);
         }
+
         int res = 1;
-        for (int num: uniques) {
+
+        for (int num : uniques) {
             int sqrt = (int) Math.sqrt(num);
             int streak = 1;
+
+            // Skip numbers that are perfect squares of a smaller number in the set
             if (sqrt * sqrt == num && uniques.contains(sqrt)) {
                 continue;
             }
+
+            // Check for square streaks
             while (num < 1000 && uniques.contains(num * num)) {
                 num *= num;
                 streak++;
             }
-            res = Math.max(res,streak);
+
+            res = Math.max(res, streak);
         }
+
         return res == 1 ? -1 : res;
     }
 }
